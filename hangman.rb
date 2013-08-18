f707e37081645df48e9b674f4b725ad6b9328b6b
Michael Swanson <mikeswanson12@msn.com>
2013-08-18 13:20:31 -0400

    sub letters in display string

--- hangman.rb
+++ hangman.rb
@@ -12,50 +12,76 @@ word_bank=['cowboys', 'indians', 'cops', 'robbers', 'water', 'bottle', 'table', 
 
 
 random_num=rand(69)
-winning_word= word_bank[random_num]
-array_of_winner=[make_array winning_word]
+winning_word= word_bank[random_num] #THIS WORKS
+edit_winner= winning_word
 
-def make_array winning_word
-  winning_word.each do |letter|
-    letter +', '
-  end
-end
-
-print array_of_winner
+puts winning_word='robber'
 
 
-#puts "Your word has been selected"
-#puts''
-#puts"To win, guess the word, by typing the word."
-#puts''
-#puts"You may also guess letters one at a time."
-#puts''
-#puts" You have " +num_of_guesses +" guesses"
-#puts""
+
+
+puts "Your word has been selected"
+puts''
+puts"To win, guess the word, by typing the word."
+puts''
+puts"You may also guess letters one at a time."
+puts''
+#puts" You have " +num_of_guesses +" guesses left"
+puts""
 
 #def num_of_guesses ##word size plus [depends on size of word]??
 #end
 
-#right_guesses=[]
-#wrong_guesses=[]
-
-#while true
-
- # puts"So far you've guessed: " +word_so_far +". Guess again"
- # guess=gets.chomp
-
- # index_of_GoodGuess = winnning_word.rindex(guess)
-
- # if guess == winning_word
- #   puts"You win!"
- # elsif guess==winning_word(index_of_GoodGuess)
- #   puts 'That is one'
- #   right_guesses.push(guess)
- #   num_of_guesses= num_of_guesses-1
- #   puts
- # end
-
+#def valid_guess guess
 #end
+
+right_guesses=[]      #for guess validation
+wrong_guesses=[]      #for guess validation
+
+empty_word_display='_' *winning_word.length  #THIS WORKS, displays unguessed letters as blanks
+word_so_far=empty_word_display                #THIS WORKS, for adding letters as guessed appropriatly
+
+#puts word_so_far                               #THIS WORKS
+
+puts"This is your word:   #{word_so_far}  . Now guess:"
+guess=gets.chomp
+
+while true
+
+
+
+  #valid_guess           #validates a guess
+
+  index_of_GoodGuess = winning_word.rindex(guess) #THIS WORKS
+
+  if guess == winning_word
+    puts"You win!"
+
+  elsif index_of_GoodGuess == nil
+  puts "SORRY! STRING UP THE NOOSE BOYS, WE GOT A LIVE ONE!"
+  wrong_guesses.push(guess)
+  #num_of_guesses= num_of_guesses-1
+
+  elsif guess == winning_word[index_of_GoodGuess]
+    puts 'The word has that letter!'
+    puts''
+    right_guesses.push(guess)
+
+    word_so_far.insert(index_of_GoodGuess, guess).slice(index_of_GoodGuess+1)
+    end
+  end
+
+
+    #num_of_guesses= num_of_guesses-1
+
+
+
+
+
+  puts"This is the word so far:   #{word_so_far}  . Guess again:"
+  guess=gets.chomp
+
+end
 
 
 
